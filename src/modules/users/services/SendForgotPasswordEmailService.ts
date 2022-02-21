c1d992df7427599090fc29f4d6049e78840f1375
Sangenis Pablo <sangenispablo@gmail.com>
2022-02-21 16:40:06 -0300

    Circuito de recuperacion de password

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,29 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import { UsersRepository } from '../typeorm/repositories/UsersRepository';
+import { UserTokensRepository } from '../typeorm/repositories/UserTokensRepository';
+
+interface IRequest {
+  email: string;
+}
+
+class SendForgotPasswordEmailService {
+  public async execute({ email }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokenRepository = getCustomRepository(UserTokensRepository);
+
+    // verifico que exista el usuario por ese mail
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Email User does not exists.');
+    }
+
+    const token = await userTokenRepository.generate(user.id);
+
+    console.log(token);
+  }
+}
+
+export default SendForgotPasswordEmailService;
